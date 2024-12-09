f9b4dbd481725e50bd79564046bbfa5ea9dcf964
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-12-09 11:00:24 +0100

    First commit.

---
+++ testimonialCardData.jsx
@@ -0,0 +1,43 @@
+import avatar1 from "./src/assets/avatar1.png";
+import avatar2 from "./src/assets/avatar2.png";
+
+
+
+export const testimonialCardData = [
+    {
+        description: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo dolor fermentum dignissim et pellentesque egestas mauris, faucibus. Tellus nisi amet non at phasellus faucibus senectus in”",
+        name: 'JOHN CARTER',
+        title: 'CEO, COMPANY',
+        icon: avatar1,
+    },
+    {
+        description: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo dolor fermentum dignissim et pellentesque egestas mauris, faucibus. Tellus nisi amet non at phasellus faucibus senectus in”",
+        name: 'JANE HOUSTON',
+        title: 'PM, COMPANY',
+        icon: avatar2,
+    },
+    {
+        description: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo dolor fermentum dignissim et pellentesque egestas mauris, faucibus. Tellus nisi amet non at phasellus faucibus senectus in”",
+        name: 'MARY JONES',
+        title: 'ROLE, COMPANY',
+        icon: avatar1,
+    },
+    {
+        description: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo dolor fermentum dignissim et pellentesque egestas mauris, faucibus. Tellus nisi amet non at phasellus faucibus senectus in”",
+        name: 'SHAWN STONES',
+        title: 'DEVELOPER, COMPANY',
+        icon: avatar2,
+    },
+    {
+        description: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo dolor fermentum dignissim et pellentesque egestas mauris, faucibus. Tellus nisi amet non at phasellus faucibus senectus in”",
+        name: 'SAM BIEBER',
+        title: 'DESIGNER, COMPANY',
+        icon: avatar1,
+    },  
+    {
+        description: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo dolor fermentum dignissim et pellentesque egestas mauris, faucibus. Tellus nisi amet non at phasellus faucibus senectus in”",
+        name: 'SOLA ADEOLA',
+        title: 'GM, COMPANY',
+        icon: avatar2,
+    },
+]
